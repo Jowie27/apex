3f328c83b9bdded10eea26ae9fdc765b5c527b5f
Brett Terpstra <me@brettterpstra.com>
2025-12-06 14:26:09 -0600

    Bump version to 0.1.16

--- include/apex/apex.h
+++ include/apex/apex.h
@@ -17,8 +17,8 @@ extern "C" {
 
 #define APEX_VERSION_MAJOR 0
 #define APEX_VERSION_MINOR 1
-#define APEX_VERSION_PATCH 15
-#define APEX_VERSION_STRING "0.1.15"
+#define APEX_VERSION_PATCH 16
+#define APEX_VERSION_STRING "0.1.16"
 
 /**
  * Processor compatibility modes
